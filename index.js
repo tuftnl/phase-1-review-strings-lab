edfe941f36fdb61875028c5759fb7a8851546cf0
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-01 16:21:14 -0400

    first attemp

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+
+let currentUser = 'Bobda'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+let firstLetter = currentUser.charAt(0)
+let shortGreeting = `Welcome, ${firstLetter}!`
